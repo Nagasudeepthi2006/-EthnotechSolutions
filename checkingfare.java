3ab9168a538e300b6602da134fcbb46908cb9a35
Nagasudeepthi2006 <nagasudeepthi18@gmail.com>
2026-05-30 16:18:37 +0530

    Update checkingfare.java

--- checkingfare.java
+++ checkingfare.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
-class Booking {
+
+class TravelBooking {
     int Uber;
     int Auto;
     int Bike;
+
     void bookRide(int uberFare) {
         Uber = uberFare;
         System.out.println("Uber Fare : Rs." + Uber);
     }
+
     void bookRide(int autoFare, String ride) {
         Auto = autoFare;
         System.out.println("Auto Fare : Rs." + Auto);
     }
+
     void bookRide(int bikeFare, String ride, int x) {
         Bike = bikeFare;
         System.out.println("Bike Fare : Rs." + Bike);
     }
+
     void checkFare() {
         if (Bike < Auto && Bike < Uber) {
             System.out.println("Bike fare is less. Person books Bike.");
@@ -29,16 +34,21 @@ class Booking {
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Booking r = new Booking();
+        TravelBooking r = new TravelBooking();
+
         System.out.print("Enter Uber Fare: ");
         int uber = sc.nextInt();
+
         System.out.print("Enter Auto Fare: ");
         int auto = sc.nextInt();
+
         System.out.print("Enter Bike Fare: ");
         int bike = sc.nextInt();
+
         r.bookRide(uber);
         r.bookRide(auto, "Auto");
         r.bookRide(bike, "Bike", 1);
+
         r.checkFare();
     }
 }
